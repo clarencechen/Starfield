0f8ba411397bd4a6795be2718a63abc26872cb4a
unknown <clchen@697-334-Thorin.sf.k12.internal>
2014-10-08 09:58:37 -0700

    initial

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle[] mol = new Particle[257];
-void setup()
+public void setup()
 {
 	noStroke();
 	size(512, 512);
@@ -10,7 +26,7 @@ void setup()
 		mol[i] = new NormalParticle();
 	}
 }
-void draw()
+public void draw()
 {
 	fill(0);
 	rect(0,0,512,512);
@@ -27,7 +43,7 @@ class NormalParticle implements Particle
 	double y;
 	double angle;
 	double speed;
-	color col;
+	int col;
 	int sz;
 	NormalParticle()
 	{
@@ -35,7 +51,7 @@ class NormalParticle implements Particle
 		this.y = 256;
 		col = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
 		angle = Math.random()*2*PI;
-		speed = Math.random()*7.5+0.5;
+		speed = Math.random()*7.5f+0.5f;
 		sz = 4;
 	}
 	public void move()
@@ -69,7 +85,7 @@ class OddballParticle implements Particle
 	double y;
 	double angle;
 	double speed;
-	color col;
+	int col;
 	int sz;
 	OddballParticle()
 	{
@@ -101,4 +117,13 @@ class OddballParticle implements Particle
 			angle = -angle;
 		}
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
